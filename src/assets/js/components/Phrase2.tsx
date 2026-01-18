980c69c5adf430fdffbbc7a399ab92f30df22967
Eyel Mordido <eyel.mordido@plankdesign.com>
2026-01-18 11:20:59 -0800

    feat(phrase2): add animation

--- src/assets/js/components/Phrase2.tsx
+++ src/assets/js/components/Phrase2.tsx
@@ -1,20 +1,39 @@
 import { useRef } from "react";
-// import { useGSAP } from "@gsap/react";
-// import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Phrase2 = () => {
 	const container = useRef<HTMLDivElement>(null);
 
-	// useGSAP(
-	// 	() => {
-	// 		if (!container.current) return;
-	// 	},
-	// 	{ scope: container },
-	// );
+	useGSAP(
+		() => {
+			if (!container.current) return;
+
+			const animatedWord = SplitText.create(".animated-word", {
+				type: "words, chars",
+			});
+
+			ScrollTrigger.create({
+				trigger: container.current,
+				start: "center center",
+				animation: gsap.to(animatedWord.chars, {
+					color: "#5bc0be",
+					stagger: 0.1,
+					duration: 0.5,
+					ease: "power2.out",
+				}),
+			});
+		},
+		{ scope: container, dependencies: [] },
+	);
 
 	return (
 		<div className="phrase-2" ref={container}>
-			<p>While ensuring content is easily accessible</p>
+			<p>
+				While ensuring content is easily <span className="animated-word">accessible</span>
+			</p>
 		</div>
 	);
 };
