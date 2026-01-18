3753e733e64b19ff65d22c44ad762cd6ce9fa24d
Eyel Mordido <eyel.mordido@plankdesign.com>
2026-01-17 19:34:13 -0800

    feat(scroll smoother): initialize on page

--- src/assets/js/components/App.tsx
+++ src/assets/js/components/App.tsx
@@ -3,6 +3,7 @@ import Phrase1 from "./Phrase1";
 import Phrase2 from "./Phrase2";
 import Phrase3 from "./Phrase3";
 import Phrase4 from "./Phrase4";
+import ScrollSmootherComponent from "./ScrollSmootherComponent";
 import { useGSAP } from "@gsap/react";
 import { DrawSVGPlugin, ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
@@ -11,13 +12,13 @@ gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger, ScrollSmoother, SplitText, use
 
 function App() {
 	return (
-		<>
+		<ScrollSmootherComponent>
 			<Header />
 			<Phrase1 />
 			<Phrase2 />
 			<Phrase3 />
 			<Phrase4 />
-		</>
+		</ScrollSmootherComponent>
 	);
 }
 
---
+++ src/assets/js/components/ScrollSmootherComponent.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode } from "react";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { useGSAP } from "@gsap/react";
+
+interface ScrollSmootherComponentProps {
+	children: ReactNode;
+}
+
+const ScrollSmootherComponent = ({ children }: ScrollSmootherComponentProps) => {
+	useGSAP(() => {
+		ScrollSmoother.create({
+			smooth: 1.25,
+			wrapper: "#smooth-wrapper",
+			normalizeScroll: true,
+		});
+	}, []);
+
+	return (
+		<div id="smooth-wrapper">
+			<div id="smooth-content">{children}</div>
+		</div>
+	);
+};
+
+export default ScrollSmootherComponent;
