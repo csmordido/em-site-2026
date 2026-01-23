847a3f4265cd8477de2fc9cbdf981578e63cdd3f
Eyel Mordido <eyel.mordido@plankdesign.com>
2026-01-23 06:59:11 -0800

    feat(link): working on scroll to

--- src/assets/js/components/Link.tsx
+++ src/assets/js/components/Link.tsx
@@ -1,5 +1,9 @@
+import { useRef } from "react";
 import AsteriskSVG from "./AsteriskSVG";
 import CircleSVG from "./CircleSVG";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import type { MouseEvent } from "react";
 interface LinkProps {
 	href: string;
 	text: string;
@@ -7,8 +11,16 @@ interface LinkProps {
 }
 
 const Link = ({ href, text, ariaLabel }: LinkProps) => {
+	const { contextSafe } = useGSAP();
+
+	const handleClick = contextSafe((event: MouseEvent, id: string) => {
+		event.preventDefault();
+		console.log("Clicked link to:", id);
+		gsap.to(window, { scrollTo: id });
+	});
+
 	return (
-		<a className="link" href={href} aria-label={ariaLabel}>
+		<a className="link" href={href} aria-label={ariaLabel} onClick={(event) => handleClick(event, href)}>
 			{text}
 			<div className="link__indicator">
 				<AsteriskSVG />
