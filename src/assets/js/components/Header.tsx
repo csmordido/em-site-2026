185c3e798465a165b711f34ce2edb577de5cc93d
Eyel Mordido <eyel.mordido@plankdesign.com>
2026-01-23 16:36:12 -0800

    update: logo, header styles

--- src/assets/js/components/Header.tsx
+++ src/assets/js/components/Header.tsx
@@ -1,9 +1,11 @@
 import Link from "./Link";
+import logo from "../../images/logo.svg";
 
 const Header = () => {
 	return (
 		<header className="header full-width">
 			<nav className="content-width">
+				<img src={logo} alt="Logo" className="header__logo" />
 				<ul className="links">
 					<li>
 						<Link text="About" href="#about" ariaLabel="Go to about section" />
